8bcfe159bae6b717b15f53baa7c2816a48b4c9fd
Anupa Devda <104296783+Anupa-123@users.noreply.github.com>
2023-03-31 13:38:07 +0530

    Update streamlit_GUI_app.py

--- streamlit_GUI_app.py
+++ streamlit_GUI_app.py
@@ -2,6 +2,7 @@ import sys
 sys.path
 ['', '/usr/local/lib/python35.zip', ...,
 '~/envs/tutorial-env/lib/python3.5/site-packages']
+!pip install yfinance
 import streamlit as st
 import yfinance as yf
 from datetime import date
